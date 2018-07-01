ed0c0f34701c31133098508ed14755b3898d9947
pmichaels <pmichaels@UK-PC0DT38G>
2018-07-01 10:35:23 +0100

    Add default seed data

--- src/UsefulSites.DataAccess/DataContext/ApplicationDbContext.cs
+++ src/UsefulSites.DataAccess/DataContext/ApplicationDbContext.cs
@@ -15,7 +15,16 @@ namespace UsefulSites.DataAccess.DataContext
         public DbSet<ResourceType> ResourceType { get; set; }
 
         public DbSet<ResourceRequest> ResourceRequest { get; set; }
-        
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ResourceType>().HasData(new Data.ResourceType()
+            {
+                Id = 1,
+                Name = "Web Site"                
+            });
+        }
     }
 }
