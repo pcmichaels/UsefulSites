417504e7a7420244bce6db6364a9eac18af430b3
pmichaels <pmichaels@UK-PC0DT38G>
2018-07-01 11:30:06 +0100

    Add new controller for displaying lists of sites

---
+++ src/UsefulSites.Web/Controllers/WebSiteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsefulSites.Web.Controllers
+{
+    public class WebSiteController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+    }
+}
