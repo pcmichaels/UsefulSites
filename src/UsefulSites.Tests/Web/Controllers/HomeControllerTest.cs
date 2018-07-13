309df0626ff64627f6cc8e1f05b2e3f053118a43
pmichaels <pmichaels@UK-PC0DT38G>
2018-07-13 07:51:20 +0100

    Add controller unit test

---
+++ src/UsefulSites.Tests/Web/Controllers/HomeControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UsefulSites.Controllers;
+using UsefulSites.DataAccess.Api;
+using UsefulSites.Web.ViewModels;
+using Xunit;
+
+namespace UsefulSites.Tests.Web.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void CallIndex_NoData()
+        {
+
+            // Arrange
+            var resourceDataAccess = Substitute.For<IResourceDataAccess>();
+            var resourceTypeDataAccess = Substitute.For<IResourceTypeDataAccess>();
+            var categoryDataAccess = Substitute.For<IResourceCategoryDataAccess>();
+
+            var homeController = new HomeController(
+                resourceTypeDataAccess, resourceDataAccess,
+                categoryDataAccess);
+
+            // Act
+            var result = homeController.Index();
+
+            // Assert
+            ViewResult view = Assert.IsType<ViewResult>(result);
+            MainViewModel viewModel = Assert.IsType<MainViewModel>(view.Model);
+            Assert.Empty(viewModel.Categories);
+            Assert.Empty(viewModel.TopResources);
+        }
+    }
+}
